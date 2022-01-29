724064c34bdc415e0f762c1b5e5aaec24be6469e
Wallace Leung <wcsl2@cam.ac.uk>
2022-01-29 13:13:55 +0000

    finished Task 1E

---
+++ Task1E.py
@@ -0,0 +1,9 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+def main():
+    stations = build_station_list()
+    print(rivers_by_station_number(stations, 9))
+
+if __name__ == "__main__":
+    main()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,15 +9,31 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 
 def rivers_with_station(stations):
-    tempSet = set()
+    temp_set = set()
     for station in stations:
-        tempSet.add(station.river)
-    return tempSet
+        temp_set.add(station.river)
+    return temp_set
 
 def stations_by_river(stations):
-    tempDict = {}
+    temp_dict = {}
     for station in stations:
-        if station.river not in tempDict:
-            tempDict[station.river] = []
-        tempDict[station.river].append(station.name)
-    return tempDict
+        if station.river not in temp_dict:
+            temp_dict[station.river] = []
+        temp_dict[station.river].append(station.name)
+    return temp_dict
+
+def sorting_function(e):
+    return e[1]
+
+def rivers_by_station_number(stations, N):
+    temp_dict = {}
+    for station in stations:
+        if station.river not in temp_dict:
+            temp_dict[station.river] = 0
+        else:
+            temp_dict[station.river] += 1
+    temp_list = list(temp_dict.items())
+    temp_list.sort(reverse=True, key=sorting_function)
+    while (N != len(temp_list) and temp_list[N-1][1] == temp_list[N][1]):
+        N += 1
+    return temp_list[:N]
