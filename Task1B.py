2e65a749579ef1b6c4b47d33d19f135c41c2b46c
py-chen6 <tc610@cam.ac.uk>
2022-01-30 01:13:20 +0000

    completed Task 1B

--- Task1B.py
+++ Task1B.py
@@ -1,9 +1,22 @@
-# Wallace is dumb
-print(1)
-print("Wallace is dumb")
-print("hello world")
-print("ok")
-print("I go to school by bus")
-def main():
-    print(0)
-    print(1+2+3)
+# Copyright (C) 2022 Robert T. Chen
+#
+# Import the relevant modules
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+def run():
+    """Requirements for Task 1B: write a program that prints a list of tuples
+    (station name, town, distance), for the 10 closest and the 10 furthest stations
+    from the Cambridge city centre, (52.2053, 0.1218)"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Build list of (station name, station town, distance from Cam city centre)
+    distance_list = stations_by_distance(stations, (52.2053, 0.1218))
+    
+    print(distance_list[:10])
+    print(distance_list[-10:])
+
+if __name__ == "__main__":
+    run()
