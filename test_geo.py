9e76227fe5eec9eb8148ab02bfa3609e2f9d1210
py-chen6 <tc610@cam.ac.uk>
2022-02-05 18:05:16 +0000

    created test_geo.py

---
+++ test_geo.py
@@ -0,0 +1,18 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def test_stations_within_radius():
+    '''Test Task1C'''
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Build list of of stations within 10 km of the Cambridge city centre
+    # In alphabetical order
+    radius_list = stations_within_radius(stations, (52.2053, 0.1218), 10)
+
+    required_output = ['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool",
+ 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton',
+ 'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
+ 
+    assert radius_list == required_output
