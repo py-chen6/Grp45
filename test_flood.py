74bfbf4be9207aee0fbe73c9414be8645ca3e694
py-chen6 <tc610@cam.ac.uk>
2022-02-27 13:30:40 +0000

    well, test 2B again

--- test_flood.py
+++ test_flood.py
@@ -3,18 +3,21 @@ from floodsystem.flood import stations_level_over_threshold
 from floodsystem.flood import stations_highest_rel_level
 
 def test_stations_level_over_threshold():
-    statA = MonitoringStation("A", None, None, None, [0.1, 2.0], None, None)
-    statB = MonitoringStation("B", None, None, None, [0.1, 2.0], None, None)
-    statC = MonitoringStation("C", None, None, None, [0.1, 2.0], None, None)
-    statD = MonitoringStation("D", None, None, None, [2.0, 0.1], None, None) # inconsistent case
-    statE = MonitoringStation("E", None, None, None, None, None, None) # inconsistent case
+    statA = MonitoringStation("A", None, None, None, [1, 2], None, None)
+    statB = MonitoringStation("B", None, None, None, [1, 2], None, None)
+    statC = MonitoringStation("C", None, None, None, [1, 2], None, None)
+    statD = MonitoringStation("D", None, None, None, [1, 2], None, None)
+    statE = MonitoringStation("E", None, None, None, [2, 1], None, None) # Inconsistent case
+    statF = MonitoringStation("F", None, None, None, None, None, None) # Inconsistent case
 
-    statA.latest_level = 1
-    statB.latest_level = 2
-    statC.latest_level = 3
+    statA.latest_level = 5
+    statB.latest_level = 6
+    statC.latest_level = 4
+    statD.latest_level = 3
     
-    stations = [statA, statB, statC, statD, statE]
+    stations = [statA, statB, statC, statD, statE, statF]
     rel_list = stations_level_over_threshold(stations, 2)
     assert isinstance(rel_list, list) # Check correct type of output
     assert isinstance(rel_list[0], tuple) # Check correct type of element within the output
-    assert rel_list == [(statC, 3)] # Check that the output is exactly right
+    assert rel_list[0] == (statB, 5) # Check that the output is sorted in descending order
+    assert len(rel_list) == 3 # statD, E, F should not be included
