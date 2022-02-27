fe91274f927ef00a992bd9ba066ee93b4531cb88
py-chen6 <tc610@cam.ac.uk>
2022-02-27 12:19:20 +0000

    submodule created for Task2B2C

---
+++ floodsystem/flood.py
@@ -0,0 +1,17 @@
+# Import the relevant modules
+from .utils import sorted_by_key
+
+def stations_level_over_threshold(stations, tol):
+    '''Requirements for Task 2B'''
+    temp_list = []
+    for station in stations:
+        if station.relative_water_level() is None or station.relative_water_level() <= tol:
+            continue
+        temp_list.append((station, station.relative_water_level()))
+        sorted_temp_list = sorted_by_key(temp_list, 1, reverse=True)
+    return sorted_temp_list
+
+def stations_highest_rel_level(stations, N):
+    '''Requirements for Task 2C'''
+    temp_list = stations_level_over_threshold(stations, 0.8)
+    return temp_list[:N]
