5e74d68bc385199f24da1832291faed717626bc1
py-chen6 <tc610@cam.ac.uk>
2022-02-27 14:37:19 +0000

    rectified requirements for 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -3,6 +3,7 @@ from .utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     '''Requirements for Task 2B'''
+    '''Returns a list of tuples'''
     temp_list = []
     for station in stations:
         if station.relative_water_level() is None or station.relative_water_level() <= tol:
@@ -13,5 +14,12 @@ def stations_level_over_threshold(stations, tol):
 
 def stations_highest_rel_level(stations, N):
     '''Requirements for Task 2C'''
-    temp_list = stations_level_over_threshold(stations, 0.8)
-    return temp_list[:N]
+    '''Returns a list of the N stations (objects)'''
+    temp_list = []
+    for station in stations:
+        if station.relative_water_level() is None:
+            continue
+        temp_list.append((station, station.relative_water_level()))
+        sorted_temp_list = sorted_by_key(temp_list, 1, reverse=True)
+    object_list = list(element[0] for element in sorted_temp_list[:N])
+    return object_list
