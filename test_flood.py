f3cb98e6c4708e3362f2722cd346c03f6d77dca0
py-chen6 <tc610@cam.ac.uk>
2022-02-27 12:32:22 +0000

    test2 for test_flood

--- test_flood.py
+++ test_flood.py
@@ -4,10 +4,12 @@ from floodsystem.flood import stations_highest_rel_level
 
 def test_stations_level_over_threshold():
     station1 = MonitoringStation(None, None, "Station 1", None, (0.15, 0.1), None, None)
-    temp_list = stations_level_over_threshold(station1, 0.8)
+    stations = list(station1)
+    temp_list = stations_level_over_threshold(stations, 0.8)
     assert temp_list == []
 
 def test_stations_highest_rel_level():
     station1 = MonitoringStation(None, None, "Station 1", None, (0.15, 0.1), None, None)
-    temp_list = stations_highest_rel_level(station1, 0.8)
+    stations = list(station1)
+    temp_list = stations_highest_rel_level(stations, 0.8)
     assert temp_list == []
